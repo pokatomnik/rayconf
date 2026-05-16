b090ca0b554550aeaa7eaf3d9d07466bd20e3572
Danlian Akhmedzianov <hugefast@gmail.com>
2026-05-16 15:15:13 +0300

    Add graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::ExitCode;
+
 use crate::cmd::cli::Cli;
 use crate::cmd::commands::Commands;
 use clap::Parser;
@@ -9,18 +11,50 @@ mod utils;
 mod v2parser;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> ExitCode {
     let cli = Cli::parse();
 
-    let error = match cli.command {
-        Commands::Add(add_params) => add_params.add().await,
-        Commands::Delete(delete_params) => delete_params.remove().await,
-        Commands::Select(select_params) => select_params.select().await,
-        Commands::Remote(remote_params) => remote_params.handle_action().await,
-        Commands::Perf(perf_params) => perf_params.start().await,
+    let command = async move {
+        match cli.command {
+            Commands::Add(add_params) => add_params.add().await,
+            Commands::Delete(delete_params) => delete_params.remove().await,
+            Commands::Select(select_params) => select_params.select().await,
+            Commands::Remote(remote_params) => remote_params.handle_action().await,
+            Commands::Perf(perf_params) => perf_params.start().await,
+        }
     };
 
-    if let Err(error) = error {
-        eprintln!("{}", error);
+    wait_for_exit(command).await
+}
+
+async fn wait_for_exit<T, E: std::fmt::Display>(
+    fut: impl Future<Output = Result<T, E>>,
+) -> ExitCode {
+    tokio::select! {
+        result = fut => {
+            match result {
+                Ok(_) => {
+                    ExitCode::SUCCESS
+                },
+                Err(error) => {
+                    eprintln!("Rayconf finished with error: {error}");
+                    ExitCode::FAILURE
+                }
+            }
+        }
+
+        signal = tokio::signal::ctrl_c() => {
+            match signal {
+                Ok(()) => {
+                    eprintln!("Graceful shutdown");
+                    ExitCode::from(130)
+                }
+
+                Err(error) => {
+                    eprintln!("failed to listen for Ctrl+C: {error}");
+                    ExitCode::FAILURE
+                }
+            }
+        }
     }
 }
